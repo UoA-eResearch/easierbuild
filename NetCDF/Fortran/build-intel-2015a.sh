c736d66f3d32d40502464407b0a10e80b2ce30ea
Sina Masoud-Ansari <s.ansari@auckland.ac.nz>
2015-07-28 13:35:32 +1200

    patch mpiexec for intel mpi

--- NetCDF/Fortran/build-intel-2015a.sh
+++ NetCDF/Fortran/build-intel-2015a.sh
@@ -3,6 +3,11 @@
 module purge
 module load NetCDF/4.3.3.1-intel-2015a
 
+# fix calls to mpiexec in test suite
+sed -i.bak 's/mpiexec /mpiexec.hydra /g' f03_test/run_f77_par_test.sh
+sed -i.bak 's/mpiexec /mpiexec.hydra /g' nf_test/run_f90_par_test.sh
+sed -i.bak 's/mpiexec /mpiexec.hydra /g' nf_test/run_f77_par_test.sh
+
 unset I_MPI_PMI_LIBRARY
 CC=$EBROOTIMPI/bin64/mpiicc FC=$EBROOTIMPI/bin64/mpiifort F77=$EBROOTIMPI/bin64/mpiifort ./configure --prefix=$NETCDFROOT --enable-parallel-tests
 make clean
